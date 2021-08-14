90e9433d08706fa668d6bb40ba215900b2e65bab
José Carneiro <contact@josecarnei.ro>
2021-08-14 13:55:44 +0100

    Adds movie partial, styles

--- node-express-templating/index.js
+++ node-express-templating/index.js
@@ -14,6 +14,10 @@ app.set('view engine', 'hbs');
 // We need to tell express where to look up our view templates
 app.set('views', __dirname + '/views');
 
+app.use(express.static('public'));
+
+app.locals.title = 'Default title for document';
+
 app.get('/', (request, response) => {
   // response.render takes the name of the view that should be rendered
   // plus a data object that contains properties that will be available to our
@@ -21,6 +25,7 @@ app.get('/', (request, response) => {
   // After express has rendered this template using the templating engine set
   // in options "view engine", the resulting HTML is going to be sent as a reponse
   response.render('index', {
+    title: 'Home Page',
     message: 'Ciao Mondo',
     article: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
   });
@@ -32,6 +37,7 @@ app.get('/helena', (request, response) => {
 
 app.get('/jose', (request, response) => {
   response.render('profile', {
+    title: "José's Profile",
     name: 'José Carneiro',
     nationality: 'Portuguese',
     location: {
@@ -42,7 +48,11 @@ app.get('/jose', (request, response) => {
       { name: 'Panda', species: 'dog', isWellBehaved: true },
       { name: 'Whiskers', species: 'cat', isWellBehaved: false }
     ],
-    favoriteBeverages: ['Water', 'Iced Coffee']
+    favoriteBeverages: ['Water', 'Iced Coffee'],
+    favoriteMovies: [
+      { title: 'The Shining', poster: '/the-shining.jpg', release: 1980 },
+      { title: '2001: A Space Odyssey', poster: '/2001.jpg', release: 1968 }
+    ]
   });
 });
 
