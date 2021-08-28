911d2b758a332c1cdecec8b3a1acc969364f4e93
José Carneiro <contact@josecarnei.ro>
2021-08-28 15:30:47 +0100

    Add node-sass-middleware and other useful packages

--- node-express-crud-operations/index.js
+++ node-express-crud-operations/index.js
@@ -5,6 +5,9 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const path = require('path');
 const Publication = require('./models/publication');
+const nodeSassMiddleware = require('node-sass-middleware');
+const serveFavicon = require('serve-favicon');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -13,7 +16,18 @@ hbs.registerPartials(path.join(__dirname, 'views/partials'));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(serveFavicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(express.static('public'));
+app.use(morgan('combined'));
+app.use(
+  nodeSassMiddleware({
+    dest: path.join(__dirname, 'public/styles'),
+    src: path.join(__dirname, 'styles'),
+    force: true,
+    outputStyle: 'extended',
+    prefix: '/styles'
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (request, response, next) => {
@@ -100,6 +114,11 @@ app.get('*', (request, response, next) => {
 // Express knows this is a catch all error handler because it takes 4 parameters
 app.use((error, request, response, next) => {
   console.log(error);
+  // 200 - Everything okay
+  // 404 - Page not found
+  // 401 - Forbiden
+  // 500 - Unknown server error
+  response.status(error.message === 'NOT_FOUND' ? 404 : 500);
   response.render('error');
 });
 
